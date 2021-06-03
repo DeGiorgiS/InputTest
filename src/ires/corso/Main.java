e37f1be0fa055621aa91f07df53f28858d5d5727
DeGiorgiS <stefano.degio@gmail.com>
2021-06-03 17:54:24 +0200

    Commit 2

--- src/ires/corso/Main.java
+++ src/ires/corso/Main.java
@@ -9,30 +9,35 @@ public class Main {
 	in = new Scanner(System.in);
 
 	// chiedo inserimento di 3 numeri
+        int[] numeri = new int[3];
         System.out.println("Inserisci un primo numero intero");
-        int a = in.nextInt();
+        numeri[0] = in.nextInt();
         System.out.println("Inserisci un secondo numero intero");
-        int b = in.nextInt();
+        numeri[1] = in.nextInt();
         System.out.println("Inserisci un terzo numero intero");
-        int c = in.nextInt();
-        System.out.println("Hai inserito: " + a + "; " + b + "; " + c);
+        numeri[2] = in.nextInt();
+        System.out.println("Hai inserito: " + numeri[0] + "; " + numeri[1] + "; " + numeri[2]);
 
-        // confronto di 3 numeri per trovarne il maggiore
+        // confronto di 3 numeri per trovarne il maggiore, stapandone anche la posizione
         int magg;
+        String posizione;
 
-        if(a>b){
-            magg = a;
+        if(numeri[1]>=numeri[2]){
+            magg = numeri[1];
+            posizione = "SECONDA";
         }
         else{
-            magg = b;
+            magg = numeri[2];
+            posizione = "TERZA";
         }
 
-        if (c>magg){
-            magg = c;
+        if (numeri[0]>=magg){
+            magg = numeri[0];
+            posizione = "PRIMA";
         }
         else{
         }
-        System.out.println("Il numero maggiore è: " + magg);
+        System.out.println("Il numero maggiore è: " + magg+ " ed occupa la "+ posizione + " posizione");
 
         //in alternativa confronto con IF annidati e salvo il maggiore.
         /*
