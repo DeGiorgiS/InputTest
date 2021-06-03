74e27e0d923cc14d7d44677fdcfb4abf5d9ef316
DeGiorgiS <stefano.degio@gmail.com>
2021-06-03 17:23:48 +0200

    Second commit

--- src/ires/corso/Main.java
+++ src/ires/corso/Main.java
@@ -1,8 +1,58 @@
 package ires.corso;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Scanner in;
+	in = new Scanner(System.in);
+
+	// chiedo inserimento di 3 numeri
+        System.out.println("Inserisci un primo numero intero");
+        int a = in.nextInt();
+        System.out.println("Inserisci un secondo numero intero");
+        int b = in.nextInt();
+        System.out.println("Inserisci un terzo numero intero");
+        int c = in.nextInt();
+        System.out.println("Hai inserito: " + a + "; " + b + "; " + c);
+
+        // confronto di 3 numeri per trovarne il maggiore
+        int magg;
+
+        if(a>b){
+            magg = a;
+        }
+        else{
+            magg = b;
+        }
+
+        if (c>magg){
+            magg = c;
+        }
+        else{
+        }
+        System.out.println("Il numero maggiore è: " + magg);
+
+        //in alternativa confronto con IF annidati e salvo il maggiore.
+        /*
+        if (a>b){
+            if (a>c){
+                magg = a;
+            }
+            else{
+                magg = c;
+            }
+        }
+        else{
+            if (b>c){
+                magg = b;
+            }
+            else{
+                magg = c;
+            }
+        }
+        System.out.println("Il numero maggiore è: " + magg);
+        */
     }
 }
